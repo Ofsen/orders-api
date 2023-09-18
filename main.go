502102365a3084cf6648bddac5c052d85eda2bea
Ofsen <ofsensmailbox@protonmail.com>
2023-09-18 22:32:10 +0200

    Graceful shutdown complete

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
+	"os/signal"
 
 	"github.com/Ofsen/orders-api/app"
 )
@@ -10,8 +12,13 @@ import (
 func main() {
 	app := app.New()
 
-	err := app.Start(context.TODO())
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	err := app.Start(ctx)
 	if err != nil {
 		fmt.Println("failed to start app:", err)
 	}
+
+	cancel()
 }
